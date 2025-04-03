6e0500c2f34905c46bbc4c0ea96ce80f785fff04
abdokhater <110694967+abdothetruk2@users.noreply.github.com>
2025-04-03 18:29:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ io.on('connection', (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80 || 3000;
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
