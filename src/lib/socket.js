4a9226a63636eddd89efae50df75ebe617b2c2df
abdokhater <110694967+abdothetruk2@users.noreply.github.com>
2025-04-04 12:12:39 +0200

    Update socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'https://localhost2.netlify.app';
-
+const SOCKET_URL = 'http://localhost:3000';
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   reconnection: true,
