d6e1836bc0a65db9be1f723045d744fbda48d0a2
abdothetruk2 <abdokhater278@gmail.com>
2025-04-04 11:34:47 +0200

    initial

--- index.js
+++ index.js
@@ -154,7 +154,11 @@ io.on('connection', (socket) => {
 });
 
 // Start server
+
 const PORT = process.env.PORT ||  3000;
+
+
+
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,10 @@
 import { io } from 'socket.io-client';
 
+<<<<<<< HEAD
 const SOCKET_URL = 'https://localhost2.netlify.app:3000';
+=======
+const SOCKET_URL = 'https://localhost2.netlify.app/';
+>>>>>>> b229c68 (initial)
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
