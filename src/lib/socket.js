f93c09a7356df7f56f3d574d8581bfd031f8e6d5
abdothetruk2 <abdokhater278@gmail.com>
2025-04-04 11:25:59 +0200

    m

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'https://localhost2.netlify.app/';
+const SOCKET_URL = 'localhost:3000';
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
