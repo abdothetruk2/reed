fa64e5bc6133eb73ddb05cb4ea6a42d630608530
abdokhater <110694967+abdothetruk2@users.noreply.github.com>
2025-04-04 12:59:28 +0200

    Update socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'https://reed-nu.vercel.app';
+const SOCKET_URL = 'http://localhost:3000';
 export const socket = io(SOCKET_URL, {
   transports: ["websocket"]
 });
